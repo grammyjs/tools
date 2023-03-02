6a99e94b03fe8bb923ced25be5a75c124d2bf279
Roj <ez@roj.im>
2023-03-02 19:06:22 +0300

    Setup fonts

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import useAsset from "ultra/hooks/use-asset.js";
+import { getFontStyles } from "./utilities/font_styles.tsx";
 
 export default function App() {
-  console.log("Hello world!");
   return (
     <html lang="en">
       <head>
@@ -11,26 +11,10 @@ export default function App() {
         <link rel="shortcut icon" href={useAsset("/favicon.ico")} />
         <link rel="stylesheet" href={useAsset("/modern-normalize.css")} />
         <link rel="stylesheet" href={useAsset("/main.css")} />
+        <style dangerouslySetInnerHTML={{ __html: getFontStyles() }} />
       </head>
       <body>
-        <main>
-          <h1>
-            <span></span>__<span></span>
-          </h1>
-          <p>
-            Welcome to <strong>Ultra</strong>. This is a barebones starter for
-            your web app.
-          </p>
-          <p>
-            Take{" "}
-            <a href="https://ultrajs.dev/docs" target="_blank">
-              this
-            </a>
-            , you may need it where you are going. It will show you how to
-            customize your routing, data fetching, and styling with popular
-            libraries.
-          </p>
-        </main>
+        <main></main>
       </body>
     </html>
   );
