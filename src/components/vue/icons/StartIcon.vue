6665e49bda9f294e2ae72bad902cd25a1c316d4f
Roj <ez@roj.im>
2023-08-03 17:05:40 +0300

    Fix one more thing

--- src/components/vue/icons/StartIcon.vue
+++ src/components/vue/icons/StartIcon.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts"></script>
 <template>
-  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M14.5 7.13398C15.1667 7.51888 15.1667 8.48113 14.5 8.86603L5.5 14.0622C4.83333 14.4471 4 13.966 4 13.1962V2.80385C4 2.03405 4.83333 1.55292 5.5 1.93782L14.5 7.13398Z"
-      fill="#D9D9D9"
-    />
+  <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
     <path
       fill-rule="evenodd"
       clip-rule="evenodd"
       d="M4 13.1962C4 13.966 4.83333 14.4471 5.5 14.0622L14.5 8.86603C15.1667 8.48113 15.1667 7.51888 14.5 7.13398L5.5 1.93782C4.83333 1.55292 4 2.03405 4 2.80385V13.1962Z"
-      fill="black"
+      fill="currentColor"
     />
   </svg>
 </template>
