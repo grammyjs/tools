27d57688cdcdad4079cbdfea089643df20d42c2a
Roj <ez@roj.im>
2024-03-23 15:56:33 +0300

    Add redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), react(), tailwind()],
+  redirects: {
+    '/': 'https://telegram.tools',
+  },
 });
